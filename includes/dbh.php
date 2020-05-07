98ae1c3bc2682507064182919808b79262b2dbd8
Tomáš Hanečák <50167956+tomashanecak@users.noreply.github.com>
2020-05-07 23:41:18 +0200

    Adding PHP

---
+++ includes/dbh.php
@@ -0,0 +1,11 @@
+<?php 
+
+    $dbServer = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+
+    $dbName = "Contact";
+
+    $conn = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);    
+
+?>
